4f650ec388d8d6e325433d97bdc00c845c1fb676
etcheekes <echeekes@gmail.com>
2023-02-08 15:08:14 +0000

    url mappings for app

---
+++ liftit/workout_planner/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # blank string indicates root of website
+    path('', views.home, name='home'), #name='home' gives a relative URL pattern to link to
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('testing/', views.testing, name='testing')
+]
