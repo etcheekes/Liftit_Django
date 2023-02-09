6ab9fd3d3bcc5a212dcc5853bcf30047d7303f61
etcheekes <echeekes@gmail.com>
2023-02-09 16:38:00 +0000

    view function handlers for browse page and delete functionality

--- liftit/workout_planner/views.py
+++ liftit/workout_planner/views.py
@@ -98,3 +98,116 @@ def error(request):
     """View function for displaying a customised error message"""
 
     return render(request, 'error.html')
+
+def browse(request):
+    """View function for the browse page of site"""
+    # identify user
+    user = request.user
+    
+    # obtain distinct values for muscle and equipment (flat = true returns list of individual values) and order in ascending
+    all_muscle = Exercises.objects.filter(user_id__exact=user.id).values_list('muscle', flat=True).distinct().order_by(Lower('muscle'))
+    all_equipment = Exercises.objects.filter(user_id__exact=user.id).values_list('equipment', flat=True).distinct().order_by(Lower('equipment'))
+
+    # display chosen exercises 
+    muscle_display = request.GET.get("exercises")
+    # once muscle group is chosen
+    if muscle_display != None:
+
+        reveal_table = 'show'
+
+        category_to_display = Exercises.objects.filter(muscle=muscle_display, user_id=user)
+
+        context = {
+            'all_muscle' : all_muscle,
+            'all_equipment' : all_equipment,
+            'category_to_display' : category_to_display,
+            'reveal_table' : reveal_table,
+            'muscle_display' : muscle_display,
+        }
+
+        return render(request, 'browse.html', context=context)
+
+    # to display exercises by equipment
+    equipment_display = request.GET.get("equipments")
+    # once equipment group is chosen
+    if equipment_display != None:
+
+        reveal_table = 'show'
+
+        category_to_display = Exercises.objects.filter(equipment=equipment_display, user_id=user)
+
+        context = {
+            'all_muscle' : all_muscle,
+            'all_equipment' : all_equipment,
+            'category_to_display' : category_to_display,
+            'reveal_table' : reveal_table,
+            'equipment_display' : equipment_display,
+        }
+
+        return render(request, 'browse.html', context=context)
+    
+    # to display exercises containing text that the user entered
+    exercise_name = request.GET.get("exercise_name")
+    # once string is chosen
+    if exercise_name != None:
+        reveal_table = 'show'
+
+        category_to_display = Exercises.objects.filter(exercise__contains=exercise_name, user_id=user)
+
+        context = {
+            'all_muscle' : all_muscle,
+            'all_equipment' : all_equipment,
+            'category_to_display' : category_to_display,
+            'reveal_table' : reveal_table,
+        }
+
+        return render(request, 'browse.html', context=context)
+
+    # initial render of page (before user searches)
+    context = {
+        'all_muscle' : all_muscle,
+        'all_equipment' : all_equipment,
+    }
+    return render(request, 'browse.html', context=context)
+
+def delete_exercise(request):
+
+    # This views serves only to delete exercises from the user's exercise table(deletion is specific to the user logged in)
+    if request.method == "POST":
+
+        # identify user
+        user = request.user
+
+        # get exercise row to delete
+        to_delete = request.POST.get("delete")
+        row_to_delete = Exercises.objects.filter(id__exact=to_delete)
+
+        # check if exercise exist in user's personal workout plan, if so then delete (todo)
+
+        # if row exists then delete from exercises table
+        if row_to_delete.exists():
+            row_to_delete.delete()
+        else:
+            error = "Exercise is not stored"
+            context = {
+                'error': error,
+                'store_url': request.get_full_path()
+                }     
+            return render(request, 'error.html', context=context)
+
+        # obtain distinct categories from user's exercises 
+        all_muscle = Exercises.objects.filter(user_id__exact=user.id).values_list('muscle', flat=True).distinct().order_by(Lower('muscle'))
+        all_equipment = Exercises.objects.filter(user_id__exact=user.id).values_list('equipment', flat=True).distinct().order_by(Lower('equipment'))
+
+        context = {
+            'all_muscle' : all_muscle,
+            'all_equipment' : all_equipment,
+        }
+
+        return render(request, 'browse.html', context=context)
+
+
+def testing(request):
+    """View function to testing purposes"""
+
+    return render(request, 'testing.html')
