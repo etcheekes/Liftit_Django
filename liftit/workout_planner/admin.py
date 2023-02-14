612e15f383f9b6e866b4b9ce9d59ddb4eafd9d99
etcheekes <echeekes@gmail.com>
2023-02-14 16:15:46 +0000

    django admin for certain pages

--- liftit/workout_planner/admin.py
+++ liftit/workout_planner/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin # for custom user table
-from .models import User
+from .models import User, Default_exercises, Exercises
 
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
+
+@admin.register(Default_exercises)
+class Default_exercises(admin.ModelAdmin):
+    # in admin view, display these columns from the Default_exercises model
+    list_display = ('default_id', 'default_exercise', 'default_muscle', 'default_equipment')
+    search_fields = ('default_exercise',)
+
+@admin.register(Exercises)
+class Exercises(admin.ModelAdmin):
+    list_display = ('id', 'exercise', 'muscle', 'equipment')
+    search_fields = ('exercise',)
