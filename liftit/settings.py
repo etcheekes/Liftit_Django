e89261750439dd4ad254483845d4b418d6e9f01b
etcheekes <echeekes@gmail.com>
2023-02-17 12:22:20 +0000

    deleted / from csrf trusted origins, cause error

--- liftit/settings.py
+++ liftit/settings.py
@@ -151,4 +151,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
-CSRF_TRUSTED_ORIGINS = ['https://lift-it.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://lift-it.up.railway.app']
